71a398c2b1ba52254aee12eb22dd084147bc7ab4
STUDENT <sriramsriram39047@gmail.com>
2026-06-12 10:41:20 +0530

    Added Variables

---
+++ var.js
@@ -0,0 +1,12 @@
+//variable declaring 
+var name ="sriram"
+console.log(name)
+
+//using let for declaraction 
+{
+let age =19
+console.log(age)
+}
+//using constant to fit the value
+const DOB = 2008
+console.log(DOB)
