ec9e3e5338f43f953cdf7fee7a97d7f73c1c1b38
STUDENT <sriramsriram39047@gmail.com>
2026-06-13 13:55:14 +0530

    //added

---
+++ Table.js
@@ -0,0 +1,18 @@
+for (let i = 1; i<=10; i++){
+    console.log(i + " X 2 =" + (i*2));
+}
+
+console.log("While Loop");
+let j = 1;
+while (j <=10) {
+    console.log(j + " X 2 =" + (j*2));
+    j++;
+}
+
+console.log("Do While");
+let m = 1;
+do {
+    
+    console.log(m + " X 2 =" + (m*2));
+    m++;
+} while (m<=10);
